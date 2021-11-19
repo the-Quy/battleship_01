57f41853e456efb5ff22bc2bc45130d2a729498f
the-Quy <88329209+the-Quy@users.noreply.github.com>
2021-11-18 23:16:31 -0500

    16 nov

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,23 @@
-import Board from './Board';
-import '../style/tablet.scss';
-
+import Board from "./Board";
+// import "../style/tablet.scss";
+import "../style/interface.scss";
+let player1 = "Player 1";
+let player2 = "Player 2";
+let turn = "Ennemy's Turn";
 
 function App() {
-  return (<div className="tablet">
-    <Board/>
-  </div>);
+    return (
+        <div>
+            <div className="title">
+                {player1} vs {player2}
+                <div>BattleSHip</div>
+                {turn}
+            </div>
+            <div className="boardArea">
+                <Board />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/component/Board.js
+++ src/component/Board.js
@@ -5,32 +5,34 @@ import Tile from "./Tile";
 const horizontalAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const verticalAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-
 export default function Board() {
-  const [allValues, setValues] = useState(Array(100).fill(null));
+    const [allValues, setValues] = useState(Array(100).fill(null));
 
-  const nextSymbole = "X";
+    const nextSymbole = "X";
 
-  function onClickTile(index) {
-    let maCopie = allValues.slice();
-    console.log(maCopie);
-    maCopie[(index)] = nextSymbole;
-    setValues(maCopie);
-  }
+    function onClickTile(index) {
+        let maCopie = allValues.slice();
+        console.log(maCopie);
+        maCopie[index] = nextSymbole;
+        setValues(maCopie);
+    }
 
-  function fabriqueTile(index) {
+    function fabriqueTile(index) {
+        return (
+            <Tile value={allValues[index]} click={() => onClickTile(index)} />
+        );
+    }
+
+    let board = []; // board creation
+
+    for (let ii = 0; ii < verticalAxis.length * horizontalAxis.length; ii++) {
+        board.push(<div className="">{fabriqueTile([ii])}</div>);
+    }
     return (
-      <Tile
-        value={allValues[(index)]}
-        click={() => onClickTile(index)}
-      />
+        <div>
+            <div className="background-board">
+                <div className="board">{board}</div>
+            </div>
+        </div>
     );
-  }
-
-  let board = []; // board creation
-
-  for (let ii = 0; ii < verticalAxis.length * horizontalAxis.length; ii++) {
-    board.push(<div className="">{fabriqueTile([ii])}</div>);
-  }
-  return <div className="board">{board}</div>;
 }
